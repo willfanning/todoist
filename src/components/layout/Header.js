d45ed7614ecd457cd304d2c6e928e93b63f5e684
Will <william.fanning1@gmail.com>
2019-09-03 18:46:33 -0500

    styling overload

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,20 +1,24 @@
-import React from 'react'; 
-import { FaPizzaSlice } from 'react-icons/fa';
+import React from "react";
+import { FaPizzaSlice } from "react-icons/fa";
 
 export const Header = () => {
-  return <header className="header" data-testid="header">
-    <nav>
-      <div className="logo">
-        <img src="/images/logo.png" alt="todoist" />
-      </div>
-      <div className="settings">
-        <ul>
-          <li>+</li>
-          <li>
-            <FaPizzaSlice />
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
-}
+  return (
+    <header className="header" data-testid="header">
+      <nav>
+        <div className="logo">
+          <img src="/images/logo.png" alt="todoist" />
+        </div>
+        <div className="settings">
+          <ul>
+            <li className="settings__add" data-testid="quick-add-task-action">
+              +
+            </li>
+            <li className="settings__darkmode" data-testid="dark-mode-action">
+              <FaPizzaSlice />
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
